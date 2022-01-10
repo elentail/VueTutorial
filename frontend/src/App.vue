631b04d1d2e0a9917a4b1a3a185c229b71cb9fca
Roadcom <mr.xeros@gmail.com>
2022-01-11 02:21:53 +0900

    frontend added

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <b-container fluid class="bv-example-row">
+      <div class="w-75 m-auto">
+      <b-row>
+        <b-col cols="6"><directive-example/></b-col>
+        <b-col cols="6"><directive-example/></b-col>
+      </b-row>   
+      </div>
+    </b-container> 
+    <chart-example/>
+    <!-- <component-example/> -->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// import ComponentExample from "./components/ComponentExample.vue";
+import DirectiveExample from "./components/DirectiveExample.vue";
+import ChartExample from "./components/ChartExample.vue"
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    // ComponentExample,
+    DirectiveExample,
+    ChartExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/ComponentExample.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>vue-code-highlight example</h2>
+    <code-highlight language="javascript">
+        
+      let isPalindrome = (word) => {
+
+      let [length, result] = [word.length, true];
+      for (let i = 0; i &lt; length / 2; i++) {
+      if (word[i] !== word[length - 1 - i]) result = false;
+
+      }
+      return result;
+
+      }
+      module.exports = isPalindrome;
+
+    </code-highlight>
+  </div>
+</template>
+
+<script>
+import CodeHighlight from "vue-code-highlight/src/CodeHighlight.vue";
+import "vue-code-highlight/themes/duotone-sea.css";
+import "vue-code-highlight/themes/window.css";
+export default {
+  name: "ComponentExample",
+  components: {
+    CodeHighlight
+  }
+};
+</script>
